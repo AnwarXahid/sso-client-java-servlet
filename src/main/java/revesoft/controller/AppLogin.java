290269130c86a7f959577541370c7b327a7d6b6c
Anwar H Zahid <mdahzahid@gmail.com>
2021-10-04 16:10:57 +0600

    new changed made for nano soft!

--- src/main/java/revesoft/controller/AppLogin.java
+++ src/main/java/revesoft/controller/AppLogin.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @WebServlet("/applogin")
 public class AppLogin extends HttpServlet {
@@ -18,6 +19,22 @@ public class AppLogin extends HttpServlet {
             ISSOAppLogin issoAppLogin = new SSOAppLogin();
             String url = issoAppLogin.getLandingUrl(request);
             System.out.println("Back from IDP!! Do as you want!!!!");
+
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while(headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                System.out.println("Header Name - " + headerName + ", Value - " + request.getHeader(headerName));
+            }
+
+            Enumeration<String> params = request.getParameterNames();
+            while(params.hasMoreElements()){
+                String paramName = params.nextElement();
+                System.out.println("Parameter Name - "+paramName+", Value - "+request.getParameter(paramName));
+            }
+
+            System.out.println("Back from IDP!! Do as you want!!!!");
+
+            System.out.println("Request body data: " + request.getParameter("token") + " %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% "  + request.getParameter("csrf_token"));
 //            response.sendRedirect(url);
             response.sendRedirect("https://103.156.52.135/prp_sso/ssologout");
         } catch (Exception ex) {
